24d48b580e3c5309509ebb0a091fd5c7d7c15f58
sam haze <mevlutkural4270@gmail.com>
2024-02-18 03:46:14 +0300

    Implement proxy routing in API Gateway

--- api-gateway/src/app.ts
+++ api-gateway/src/app.ts
@@ -1,16 +1,53 @@
 import express, { json } from "express";
 import config from "./config";
 import httpStatus from "http-status";
+import { createProxyMiddleware } from "http-proxy-middleware";
 
 config();
 
-const PORT = process.env.PORT || 3000;
 const app = express();
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  return res.status(httpStatus.OK).send("API GATEWAY hadi lan ordan!!!!");
+const userServiceURL = "http://users-service:3001";
+const productServiceURL = "http://products-service:3002";
+const orderServiceURL = "http://orders-service:3003";
+
+const userProxy = createProxyMiddleware({
+  target: userServiceURL,
+  changeOrigin: true,
+  pathRewrite: {
+    "^/users": "",
+  },
 });
 
+const productProxy = createProxyMiddleware({
+  target: productServiceURL,
+  changeOrigin: true,
+  pathRewrite: {
+    "^/products": "",
+  },
+});
+
+const orderProxy = createProxyMiddleware({
+  target: orderServiceURL,
+  changeOrigin: true,
+  pathRewrite: {
+    "^/orders": "",
+  },
+});
+
+app.get("/", (req, res) => {
+  return res.status(httpStatus.OK).send("API GATEWAY");
+});
+
+app.use("/users", userProxy);
+app.use("/products", productProxy);
+app.use("/orders", orderProxy);
+
 app.listen(PORT, () => {
+  app.all("*", (_, res) => {
+    return res.status(httpStatus.NOT_FOUND).send("404 NOT FOUND");
+  });
+
   console.log(`api gateway listening on http://localhost:${PORT}`);
 });
--- users-service/src/app.ts
+++ users-service/src/app.ts
@@ -11,6 +11,11 @@ app.get("/", (req, res) => {
   return res.status(httpStatus.OK).send("USERS SERVICE");
 });
 
+
+// app.get("/users", (req, res) => {
+//   return res.status(httpStatus.OK).send("USERS SERVICE");
+// });
+
 app.listen(PORT, () => {
   console.log(`users service listening on http://localhost:${PORT}`);
 });
